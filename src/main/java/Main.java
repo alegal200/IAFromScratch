9651c368a8359e7dcaf2f773b2c95646d5644d53
Ghali9 <el.elboukilielhassani@student.hepl.be>
2022-04-04 02:21:08 +0200

    - Perceptron descente du gradiant avec table Logique ET : Fait - Perceptron adaline : Presque fini - Jeu de test Menu pour Perc. DG et Adal : fait

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-import PECEPTRON.PerceptronSimple;
+import PECEPTRON.*;
 
 import java.util.Scanner;
 
@@ -14,6 +14,12 @@ public class Main {
                 case 1:
                     Essai1();
                     break;
+                case 2:
+                    Essai2();
+                    break;
+                case 3:
+                    Essai3();
+                    break;
                 default:
                     System.out.println("Choix incorrect");
                     break;
@@ -26,6 +32,8 @@ public class Main {
     private static int Menu() {
         System.out.println("**********Menu*********");
         System.out.println("1. Perceptron Mise au point");
+        System.out.println("2. Perceptron Descente du Gradiant");
+        System.out.println("3. Perceptron ADALINE");
         System.out.println("***********************");
         System.out.print("choix :");
         Scanner sc = new Scanner(System.in);
@@ -39,6 +47,22 @@ public class Main {
         int[] Output = {0, 0, 0, 1};
 
         PerceptronSimple p = new PerceptronSimple();
+        p.Perceptron(Input, Output);
+    }
+
+    private static void Essai2() {
+        int[][] Input = {{1, 0, 0}, {1, 0, 1}, {1, 1, 0}, {1, 1, 1}}; //Le premier 1 est l'entrée fictive
+        int[] Output = {-1, -1, -1, 1};
+
+        PerceptronDG p = new PerceptronDG();
+        p.Perceptron(Input, Output);
+    }
+
+    private static void Essai3() {
+        int[][] Input = {{1, 0, 0}, {1, 0, 1}, {1, 1, 0}, {1, 1, 1}}; //Le premier 1 est l'entrée fictive
+        int[] Output = {-1, -1, -1, 1};
+
+        PerceptronADALINE p = new PerceptronADALINE();
         p.Perceptron(Input, Output);
     }
 
